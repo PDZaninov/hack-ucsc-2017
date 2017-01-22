5dbfd0c3ea81fbfe190d28fc46b0bc06ae105c58
Petar Zaninovich <pzaninov@ucsc.edu>
2017-01-21 18:15:37 -0800

    picture for last

--- controllers/default.py
+++ controllers/default.py
@@ -23,7 +23,7 @@ def mainview():
     return dict(message=T('Hi'))
 
 def profile():
-    return dict(picture=db(db.post).select(db.post.image).first().image)
+    return dict(picture=db(db.post).select(db.post.image).last().image)
 
 
 def user():
